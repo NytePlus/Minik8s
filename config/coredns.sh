9ed6d5387755512e757518b9bb85396635d0bf58
LostofMoon <1833775406@qq.com>
2025-06-11 01:34:59 +0800

    no use file

--- config/coredns.sh
+++
@@ -1,7 +0,0 @@
-docker run -d \
-  --name coredns \
-  -p 54:53/udp \
-  -p 54:53/tcp \
-  -v /root/workspace2/k8s_group_4/config/Corefile:/Corefile \
-  --network bridge \
-  coredns/coredns:latest
