96cb8c78d2adceac4ee83a2c169c164526aa3c3b
LostofMoon <1833775406@qq.com>
2025-06-12 01:11:58 +0800

    save for debug

--- testFile/serverless/zip-function/input/input.py
+++ testFile/serverless/zip-function/input/input.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-def input_handler(context: Dict) -> Dict:
+def input_handler(event: Dict, context: Dict) -> Dict:
     """
     输入节点handler，验证输入格式是否符合要求
 
@@ -31,26 +31,27 @@ def input_handler(context: Dict) -> Dict:
         }
 
     # 如果是generation格式
-    if 'max_length' in context or 'temperature' in context:
-        valid = True
-        if 'max_length' in context and not isinstance(context['max_length'], int):
-            valid = False
-        if 'temperature' in context and not isinstance(context['temperature'], (int, float)):
-            valid = False
+    # if 'max_length' in context or 'temperature' in context:
+    #     valid = True
+    #     if 'max_length' in context and not isinstance(context['max_length'], int):
+    #         valid = False
+    #     if 'temperature' in context and not isinstance(context['temperature'], (int, float)):
+    #         valid = False
 
-        if 'chat_history' in context:
-            valid = False
+    #     if 'chat_history' in context:
+    #         valid = False
 
-        return {
-            "error": "invalid input context",
-        }
+    #     if valid is False:
+    #         return {
+    #             "error": "invalid input context",
+    #         }
 
-    # 如果是chat格式
-    if 'chat_history' in context:
-        chat_history = context['chat_history']
-        if not isinstance(chat_history, list):
-            return {
-                "error": "invalid input context",
-            }
+    # # 如果是chat格式
+    # if 'chat_history' in context:
+    #     chat_history = context['chat_history']
+    #     if not isinstance(chat_history, list):
+    #         return {
+    #             "error": "invalid input context",
+    #         }
 
     return context
